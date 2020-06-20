355714177fe293482647075fc85e5a3c7d44b953
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-06-20 17:34:27 +0300

    10/10??

--- script.js
+++ script.js
@@ -143,7 +143,7 @@
 //     str += ss;
 
 //     return str;
-// }
+// }                                    /*мені вона теж не подобається але я поки хз як зробити краще*/
 
 // console.log(calcTime(20723));
 
@@ -151,4 +151,38 @@
 
 
 
+
+/*Написать функцию, которая считает разницу между датами. Функция принимает 6 параметров, которые описывают 2 даты, и возвращает результат в виде строки «чч:мм:сс». При выполнении задания используйте функции из предыдущих 2-х заданий: сначала обе даты переведите в секунды, узнайте разницу в секундах, а потом разницу переведите обратно в «чч:мм:сс»*/
+
+
+
+// /*Convert to seconds function*/
+// let calcSecs = (h, m = 0 ,s = 0) => h * 3600 + m * 60 + s;
+
+// /*Calculating difference between two time data*/
+// let diff;
+// function calcDiff(h1, m1, s1, h2, m2, s2) {
+//     diff = Math.abs(calcSecs(h2, m2, s2) - calcSecs(h1, m1, s1));
+//     return diff
+// }
+
+// calcDiff(8, 20, 10, 5, 48, 34);
+
+
+// /*Convertation second to "hh.mm.ss"*/
+// function calcTime(s) {
+//     let str = "";
+//     let hh = Math.floor(s/3600);
+//     str += hh;
+//     str += ":";
+//     let mm = Math.floor((s - hh*3600)/60);
+//     str += mm;
+//     str += ":";
+//     let ss = s - hh*3600 - mm*60;
+//     str += ss;
+
+//     return str;
+// }
+
+// console.log(calcTime(diff));                    /*  It works! D:   */
 
