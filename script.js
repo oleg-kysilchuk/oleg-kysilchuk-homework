f540216c3a5e55ca04fac338d2e4051c4854766e
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-07-01 17:12:56 +0300

    2/3

--- script.js
+++ script.js
@@ -60,65 +60,50 @@ document.write(`Длина окружности: ~${Math.round(c.circleLength())
 
 Продемонстрировать работу написанных методов.*/ 
 
-// printText(text) {
-//     let txt = "";
-//     for(let i = 0; i < this.ink/0.5;i++) {
-//         this.ink -= 0.5;
-//         txt += text[i];
-//         if (text[i] == " ") {
-//             this.ink += 0.5;
-//         } else if(txt.length == text.length) {
-//             break;
-//         }
-//     }
-    
-//     document.write(`<p style="color: ${this.color}">${txt}<p>`);
-// }
 
 
+class Marker {
+    constructor(color, ink = 100) {
+        this.color = color;
+        this.ink = ink;
+    }
 
-// class Marker {
-//     constructor(color, ink = 100) {
-//         this.color = color;
-//         this.ink = ink;
-//     }
-
-//     printText(text) {
-//         let txt = "";
-//         for(let i = 0; i < text.length;i++) {
-//             this.ink -= 0.5;
-//             txt += text[i];
-//             if (text[i] == " ") {
-//                 this.ink += 0.5;
-//             } else if(this.ink === 0 || txt.length === text.length) {
-//                 break;
-//             }
-//         }
+    printText(text) {
+        let txt = "";
+        for(let i = 0; i < text.length;i++) {
+            this.ink -= 0.5;
+            txt += text[i];
+            if (text[i] == " ") {
+                this.ink += 0.5;
+            } else if(this.ink === 0 || txt.length === text.length) {
+                break;
+            }
+        }
         
-//         document.write(`<p style="color: ${this.color}">${txt}<p>`);
-//     }
-// }
+        document.write(`<p style="color: ${this.color}">${txt}<p>`);
+    }
+}
 
-// let m = new Marker("black")
+let m = new Marker("black")
 
-// m.printText("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, impedit hic natus nemo fuga eum sint eius voluptate officiis esse porro omnis aspernatur expedita quae vitae voluptatibus voluptatem adipisci alias id corrupti soluta asperiores temporibus doloremque harum. Asperiores odio placeat quod iure necessitatibus, a quia magnam fuga suscipit optio numquam, nisi totam enim beatae amet impedit ipsam consectetur velit facilis excepturi, fugit reiciendis et exercitationem? Sint maiores placeat, eveniet accusamus ab a optio voluptatibus eos veritatis explicabo iusto dolorum voluptates porro quidem hic at fuga. Doloremque nihil magnam, obcaecati facilis voluptatum est, soluta in minus fugiat, neque necessitatibus recusandae nulla.");
-// debugger
+m.printText("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, impedit hic natus nemo fuga eum sint eius voluptate officiis esse porro omnis aspernatur expedita quae vitae voluptatibus voluptatem adipisci alias id corrupti soluta asperiores temporibus doloremque harum. Asperiores odio placeat quod iure necessitatibus, a quia magnam fuga suscipit optio numquam, nisi totam enim beatae amet impedit ipsam consectetur velit facilis excepturi, fugit reiciendis et exercitationem? Sint maiores placeat, eveniet accusamus ab a optio voluptatibus eos veritatis explicabo iusto dolorum voluptates porro quidem hic at fuga. Doloremque nihil magnam, obcaecati facilis voluptatum est, soluta in minus fugiat, neque necessitatibus recusandae nulla.");
+debugger
 
-// document.write(`<p>Осталось заряда маркера: ${m.ink}</p>`);
+document.write(`<p>Осталось заряда маркера: ${m.ink}</p>`);
 
 
 
-// class RefillMarker extends Marker {
-//     constructor (color, ink) {
-//         super(color, ink);
-//     }
+class RefillMarker extends Marker {
+    constructor (color, ink) {
+        super(color, ink);
+    }
 
-//     refillInk(newInk) {
-//         this.ink += newInk > 100 ? this.ink === 100 : this.ink += newInk;
-//     }
-// }
+    refillInk(newInk) {
+        this.ink += newInk > 100 ? this.ink === 100 : this.ink += newInk;
+    }
+}
 
-// let r = new RefillMarker("black", m.ink);
-// r.refillInk(80);
+let r = new RefillMarker("black", m.ink);
+r.refillInk(80);
 
-// document.write(`<p>Маркер был заправлен до: ${r.ink}</p>`);
+document.write(`<p>Маркер был заправлен до: ${r.ink}</p>`);
