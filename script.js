18e3156eec5ba9b2c3aa69142cf598c6993e8adf
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-07-01 20:37:21 +0300

    pererobyv

--- script.js
+++ script.js
@@ -1,282 +1,313 @@
 /*1. Создать массив «Список покупок». Каждый элемент массива является объектом, который содержит название продукта, необходимое количество и куплен или нет. Написать несколько функций для работы с таким массивом.*/
 
 
-// let shoppingList = [];
+let shoppingList = [];
 
-// function newPurchase(product, amount, isBought) {
+function newPurchase(product, amount, isBought) {
 
-//     let purchase = {
-//             product: product,
-//             amount: amount,
-//             isBought: isBought
-//     }
+    let purchase = {
+            product: product,
+            amount: amount,
+            isBought: isBought
+    }
 
-//     shoppingList.push(purchase);
-// }
+    shoppingList.push(purchase);
+}
 
-// newPurchase("lemon", 2, true);
-// newPurchase("bread", 1, false);
-// newPurchase("salt", 5, false);
-// newPurchase("water", 4, true);
-// newPurchase("meat", 3, false);
+newPurchase("lemon", 2, true);
+newPurchase("bread", 1, false);
+newPurchase("salt", 5, false);
+newPurchase("water", 4, true);
+newPurchase("meat", 3, false);
 
 
 /*Вывод всего списка на экран таким образом, чтобы сначала шли некупленные продукты, а потом – купленные.*/
 
-// function sortByIsBought(arr) {
-//     arr.sort((a) => a.isBought === false ? -1 : 1);
-//     arr.forEach(item => {
-//         document.write(`Продукт: ${item.product}<br>Количество: ${item.amount}<br>Куплен или нет: ${item.isBought}<br><hr>`);
-//     });
-// }
+function sortByIsBought(arr) {
+    arr.sort((a) => a.isBought === false ? -1 : 1);
+    arr.forEach(item => {
+        document.write(`Продукт: ${item.product}<br>Количество: ${item.amount}<br>Куплен или нет: ${item.isBought}<br><hr>`);
+    });
+}
 
-// sortByIsBought(shoppingList);
+sortByIsBought(shoppingList);
 
-// document.write(`<hr><hr>`);
+document.write(`<hr><hr>`);
 
 
 /*Добавление покупки в список. Учтите, что при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую.*/
 
-// let newPurch = {};
-
-// function someNewPurch(product, amount, isBought) {
-
-//     newPurch = {
-//         product: product,
-//         amount: amount,
-//         isBought: isBought
-//     }
-
-// }
 
 
+function addNewPurch(arr, product, amount ) {
+    const good = arr.find((item) => item.product === product);
 
-// function addNewPurch(arr, obj) {
-//     arr.find((item) => {
-//         if(item.product == obj.product) {
-//             item.amount += obj.amount;
-//         }
-//     })
-//     /*я хз*/
-// }
+    if(good && !good.isBought) {
+        good.amount += amount;
+    } else if(good && good.isBought) {
+        good.amount = amount;
+        good.isBought = false;
+    } else {
+        arr.push({ product, amount, isBought: false});
+    }
+}
 
-// someNewPurch("bread", 1, false);
-// addNewPurch(shoppingList, newPurch);
+addNewPurch(shoppingList, "bread", 1);
+addNewPurch(shoppingList, "fish", 3);
+addNewPurch(shoppingList, "water", 5);
+sortByIsBought(shoppingList);
+
+document.write(`<hr><hr>`);
 
 
 /*Покупка продукта. Функция принимает название продукта и отмечает его как купленный.*/
 
 
-// function buyProduct(arr, prodName) {
-//     arr.forEach((item) => {
-//         if(item.product === prodName) {
-//             item.isBought === true;
-//         }
-//     })
+function buyProduct(arr, product) {
+    arr.find((item) => {
+        if(item.product === product) {
+            item.isBought = true;
+        }
+    })
+}
 
-//     return shoppingList;    /*я хз*/
-// }
+buyProduct(shoppingList, "salt");
+sortByIsBought(shoppingList);
 
-// buyProduct(shoppingList, "salt");
-// console.log(shoppingList);
-
+document.write(`<hr><hr>`);
+document.write(`<hr><hr>`);
 
 
 
 /*2. Создать массив, описывающий чек в магазине. Каждый элемент массива состоит из названия товара, количества и цены за единицу товара. Написать следующие функции:*/
 
-// let purchasesList = [];
+let purchasesList = [];
 
-// function newPurchase(good, amount, price) {
+function newPurch(good, amount, price) {
 
-//     let purchase = {
-//             good: good,
-//             amount: amount,
-//             price: price
-//     }
+    let purchase = {
+            good: good,
+            amount: amount,
+            price: price
+    }
 
-//     purchasesList.push(purchase);
-// }
+    purchasesList.push(purchase);
+}
 
-// newPurchase("laptop", 3, 2500);
-// newPurchase("smartphone", 4, 2000);
-// newPurchase("tv", 1, 5000);
-// newPurchase("headphones", 2, 500);
-// newPurchase("xbox", 1, 3000);
+newPurch("laptop", 3, 2500);
+newPurch("smartphone", 4, 2000);
+newPurch("tv", 1, 5000);
+newPurch("headphones", 2, 500);
+newPurch("xbox", 1, 3000);
+
 
 // /*Распечатка чека на экран;*/
 
-// purchasesList.forEach((item) => {
-//     document.write(`Товар: ${item.good}<br>Количество: ${item.amount}<br>Цена за штуку: ${item.price}<br><hr>`);
-// });
+function printChecks(arr) {
+    arr.forEach((item) => {
+        document.write(`Товар: ${item.good}<br>Количество: ${item.amount}<br>Цена за штуку: ${item.price}<br><hr>`);
+    });
+}
+
+printChecks(purchasesList);
+
 
 // /*Подсчет общей суммы покупки;*/
 
-// let totalPrice = purchasesList.reduce((sum, current) => sum + (current.price * current.amount),0);
-// document.write(`Сумма покупки: ${totalPrice}<br><hr>`);
+let totalPrice;
+
+function getTotalCost(arr) {
+    totalPrice = arr.reduce((sum, current) => sum + (current.price * current.amount),0);
+    document.write(`Сумма покупки: ${totalPrice}<br><hr>`);
+}
+
+getTotalCost(purchasesList);
+
 
 // /*Получение самой дорогой покупки в чеке;*/
 
 // /*найвища ціна за одиницю товару*/
 
-// let mostExpensGood = purchasesList.sort((a, b) => a.price > b.price ? -1 : 1);
-// document.write(`Самый дорогой товар в чеке: ${mostExpensGood[0].good}<br>Цена: ${mostExpensGood[0].price}<br><hr>`);
+function getAverageCost(arr) {
+    let mostExpensGood = arr.sort((a, b) => a.price > b.price ? -1 : 1);
+    document.write(`Самый дорогой товар в чеке: ${mostExpensGood[0].good}<br>Цена: ${mostExpensGood[0].price}<br><hr>`);
+}
+
+getAverageCost(purchasesList);
+
 
 // /*найвища ціна за покупку(ціна*кількість)*/
 
-// let mostExpensPurch = purchasesList.sort((a, b) => a.price*a.amount > b.price*b.amount ? -1 : 1);
-// document.write(`Самая дорогая покупка: ${mostExpensPurch[0].good}<br>В количестве: ${mostExpensPurch[0].amount}<br>Полная цена: ${mostExpensPurch[0].price * mostExpensPurch[0].amount}<br><hr>`);
+function getAverageCostByAmount(arr) {
+    let mostExpensPurch = arr.sort((a, b) => a.price*a.amount > b.price*b.amount ? -1 : 1);
+    document.write(`Самая дорогая покупка: ${mostExpensPurch[0].good}<br>В количестве: ${mostExpensPurch[0].amount}<br>Полная цена: ${mostExpensPurch[0].price * mostExpensPurch[0].amount}<br><hr>`);
+}
+
+getAverageCostByAmount(purchasesList);
+
 
 // /*Подсчет средней стоимости одного товара в чеке.*/
 
-// let goodsAmount = purchasesList.reduce((sum, current) => (sum + current.amount),0);
-// document.write(`Средняя стоимость одного товара: ~${Math.round(totalPrice/goodsAmount)}<br><hr>`);
+function getOnePurchAverageCost(arr) {
+    let goodsAmount = arr.reduce((sum, current) => (sum + current.amount),0);
+    document.write(`Средняя стоимость одного товара: ~${Math.round(totalPrice/goodsAmount)}<br><hr>`);
+}
 
+getOnePurchAverageCost(purchasesList);
 
+document.write(`<hr><hr>`);
 
 
 /*3. Создать массив CSS-стилей (цвет, размер шрифта, выравнивание, подчеркивание и т. д.). Каждый элемент массива – это объект, состоящий из двух свойств: название стиля и значение стиля. Написать функцию, которая принимает массив стилей и текст, и выводит этот текст с помощью document.write() в тегах <p></p>, добавив в открывающий тег атрибут style со всеми стилями, перечисленными в массиве.*/
 
-// let styles = [];
+let styles = [];
 
-// function newStyle(property, value) {
+function newStyle(property, value) {
 
-//     let style = {
-//             property,
-//             value
-//     }
+    let style = {
+            property,
+            value
+    }
 
-//     styles.push(style);
-// }
+    styles.push(style);
+}
 
-// newStyle("color", "red");
-// newStyle("font-size", "20px");
-// newStyle("font-weight", "600");
-// newStyle("text-align", "center");
-// newStyle("text-decoration", "underline");
-
-// let stl = "";
-// styles.forEach((item) => {stl += `${item.property}` + ": " + `${item.value}` + "; ";});
-
-// function printText(cssStyles, text) {
-//     document.write(`<p style="${cssStyles}">${text}<p>`);
-// }
-
-// printText(stl, "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi deleniti totam asperiores officia, ut illum dolorem modi! Iste aliquid quasi eligendi reprehenderit impedit at, dignissimos explicabo enim odio itaque facilis iusto repudiandae doloribus minima blanditiis aperiam, earum totam, nobis atque. Architecto, non amet! Inventore alias ullam unde sit ratione rerum.");
+newStyle("color", "orange");
+newStyle("font-size", "20px");
+newStyle("font-weight", "600");
+newStyle("text-align", "center");
+newStyle("text-decoration", "underline");
 
 
 
+function printText(arr, text) {
+    let stylesheet = arr.map((item) => `${item.property}: ${item.value}`);
+    stylesheet = stylesheet.join(';');
+    document.write(`<p style="${stylesheet}">${text}<p>`);
+}
+
+
+printText(styles, "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi deleniti totam asperiores officia, ut illum dolorem modi! Iste aliquid quasi eligendi reprehenderit impedit at, dignissimos explicabo enim odio itaque facilis iusto repudiandae doloribus minima blanditiis aperiam, earum totam, nobis atque. Architecto, non amet! Inventore alias ullam unde sit ratione rerum.");
+
+
+document.write(`<hr><hr><hr>`);
 
 
 
 /* 4.Создать массив аудиторий академии. Объект-аудитория состоит из названия, количества посадочных мест (от 10 до 20) и названия факультета, для которого она предназначена. Написать несколько функций для работы с ним:*/
 
-// let classroomsList = [];
+let classroomsList = [];
 
-// function newClassroom(crName, pAmount, faculty) {
+function newClassroom(crName, pAmount, faculty) {
 
-//     let classroom = {
-//             crName: crName,
-//             pAmount: pAmount,
-//             faculty: faculty
-//     }
+    let classroom = {
+            crName: crName,
+            pAmount: pAmount,
+            faculty: faculty
+    }
 
-//     classroomsList.push(classroom);
-// }
+    classroomsList.push(classroom);
+}
 
-// newClassroom("gamma", 10, "physics");
-// newClassroom("alpha", 20, "mathematics");
-// newClassroom("beta", 15, "chemistry");
-// newClassroom("delta", 12, "physics");
-// newClassroom("zeta", 17, "mathematics");
-// newClassroom("echo", 16, "chemistry");
+newClassroom("gamma", 10, "physics");
+newClassroom("alpha", 20, "mathematics");
+newClassroom("beta", 15, "chemistry");
+newClassroom("delta", 12, "physics");
+newClassroom("zeta", 17, "mathematics");
+newClassroom("echo", 16, "chemistry");
 
 
 // /*Вывод на экран всех аудиторий;*/
 
-// classroomsList.forEach((item) => {
-//     document.write(`Аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
-// });
+function showAllClassrooms(arr) {
+    arr.forEach((item) => {
+        document.write(`Аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
+    });
+}
 
-// document.write(`<hr><hr>`);
+showAllClassrooms(classroomsList);
+
+
+document.write(`<hr>`);
 
 // /*Вывод на экран аудиторий для указанного факультета;*/
 
-// function getRoomByFacl(facultyName) {
+function getRoomByFacl(arr, facultyName) {
     
-//     let acceptableRooms = [];
+    let acceptableRooms = [];
 
-//     classroomsList.find((item) => {
-//         if(item.faculty == facultyName) {
-//             acceptableRooms.push(item);
-//         }
-//     })
+    arr.find((item) => {
+        if(item.faculty == facultyName) {
+            acceptableRooms.push(item);
+        }
+    })
 
-//     acceptableRooms.forEach((item) => {
-//         document.write(`Для факультета ${facultyName} подходит аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br><hr>`);
-//     });
+    acceptableRooms.forEach((item) => {
+        document.write(`Для факультета ${facultyName} подходит аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br><hr>`);
+    });
 
-//     acceptableRooms = [];
-// }
+    acceptableRooms = [];
+}
 
-// getRoomByFacl("mathematics");
-// getRoomByFacl("physics");
+getRoomByFacl(classroomsList, "mathematics");
+getRoomByFacl(classroomsList, "physics");
 
-// document.write(`<hr><hr>`);
+document.write(`<hr>`);
 
 
 // /*Функция сортировки аудиторий по количеству мест;*/
 
-// function sortByPlacesAmount(arr) {
-//     arr.sort((a, b) => a.pAmount > b.pAmount ? -1 : 1);
-//     arr.forEach((item) => {
-//         document.write(`Аудитория: ${item.crName} имеет количество мест: ${item.pAmount}<br><hr>`);
-//     });
-// }
+function sortByPlacesAmount(arr) {
+    arr.sort((a, b) => a.pAmount > b.pAmount ? -1 : 1);
+    arr.forEach((item) => {
+        document.write(`Аудитория: ${item.crName} имеет количество мест: ${item.pAmount}<br><hr>`);
+    });
+}
 
-// sortByPlacesAmount(classroomsList);
+sortByPlacesAmount(classroomsList);
 
-// document.write(`<hr><hr>`);
+document.write(`<hr>`);
+
 
 // /*Функция сортировки аудиторий по названию (по алфавиту).*/
 
-// function sortByAlfabet(arr) {
-//     arr.sort((a, b) => a.crName < b.crName ? -1 : 1);
-//     arr.forEach((item) => {
-//         document.write(`Аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
-//     });
-// }
+function sortByAlfabet(arr) {
+    arr.sort((a, b) => a.crName < b.crName ? -1 : 1);
+    arr.forEach((item) => {
+        document.write(`Аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
+    });
+}
 
-// sortByAlfabet(classroomsList);
+sortByAlfabet(classroomsList);
 
-// document.write(`<hr><hr>`);
+document.write(`<hr>`);
 
 
 // /*Вывод на экран только тех аудиторий, которые подходят для переданной группы. Объект-группа состоит из названия, количества студентов и названия факультета;*/
 
-// let someGroup = {};
+let someGroup = {};
 
-// function createGroupObj(groupName, studentsAmount, facultyName) {
+function createGroupObj(groupName, studentsAmount, facultyName) {
 
-//     someGroup = {
-//         groupName: groupName,
-//         studentsAmount: studentsAmount,
-//         facultyName: facultyName
-//     }
-// }
-
-
-// function getClassroomForGroup(arr,obj) {
-//     arr.forEach((item) => {
-//         if(item.faculty == obj.facultyName && item.pAmount >= obj.studentsAmount) {
-//             document.write(`Для данной группы подходит аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
-//         }
-//     })
-
-// }
-
-// createGroupObj("group413", 12, "chemistry" );
-// getClassroomForGroup(classroomsList, someGroup);
+    someGroup = {
+        groupName: groupName,
+        studentsAmount: studentsAmount,
+        facultyName: facultyName
+    }
+}
 
 
-// document.write(`<hr><hr>`);
+function getClassroomForGroup(arr,obj) {
+    arr.forEach((item) => {
+        if(item.faculty == obj.facultyName && item.pAmount >= obj.studentsAmount) {
+            document.write(`Для данной группы подходит аудитория: ${item.crName}<br>Количество мест: ${item.pAmount}<br>Факультет: ${item.faculty}<br><hr>`);
+        }
+    })
+
+}
+
+createGroupObj("group413", 12, "chemistry" );
+getClassroomForGroup(classroomsList, someGroup);
+
+
+document.write(`<hr><hr>`);
