a073a96f1888cf4e8f6fcdc054574af39954fbe3
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-07-05 21:45:05 +0300

    1/3

---
+++ script.js
@@ -0,0 +1,30 @@
+/*Создать HTML-страницу для отображения/редактирования текста. При открытии страницы текст отображается с помощью тега div. При нажатии Ctrl + E, вместо div появляется textarea с тем же текстом, который теперь можно редактировать. При нажатии Ctrl + , вместо textarea появляется div с уже измененным текстом. Не забудьте выключить поведение по умолчанию для этих сочетаний клавиш.*/
+
+
+const div = document.createElement('div');
+div.classList.add('div-text');
+div.innerText = "Text";
+document.body.append(div);
+
+const txtarea = document.createElement('textarea');
+txtarea.classList.add('area');
+txtarea.setAttribute('autofocus', 'autofocus');
+
+window.addEventListener('keydown', function (event) {
+    if(event.ctrlKey && event.key == "e") {
+        event.preventDefault();
+        div.remove();
+        document.body.append(txtarea);
+        txtarea.textContent = div.textContent;
+    }
+})
+
+
+window.addEventListener('keydown', function (event) {
+    if(event.ctrlKey && event.key == "=") {
+        event.preventDefault();
+        txtarea.remove();
+        document.body.append(div);
+        div.textContent = txtarea.value;
+    }
+})
