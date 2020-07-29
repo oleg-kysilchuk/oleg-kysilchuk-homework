9fa25be3ee8dd682cb61928e4054952feb3c8746
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-07-29 17:07:35 +0300

    storage

--- script.js
+++ script.js
@@ -2,22 +2,16 @@ const resCont = document.querySelector('.results');
 const srchForm = document.querySelector('#search-form');
 const pagination = document.querySelector('.pagination');
 const detailCont = document.querySelector('.movie-details');
-const typesArray = [...document.querySelectorAll('.type-btn')];
-
+const favBlock = document.querySelector('.favourites');
+const showFav = document.querySelector('.show-fav');
+const fMovies = document.querySelector('.fav-movies');
+const li = document.createElement('li');
+const clearBtn = document.querySelector('.clear');
+let favoutiteMovies = [];
 const apikey = "c549391b";
-
-let t = "";
-
-typesArray.forEach((item) => {
-  item.addEventListener('click', function() {
-    item.classList.toggle('choosed');
-    return t = item.getAttribute("data-type");
-  })
-})
 
 
 const showError = () => {
-  const li = document.createElement('li');
   li.classList.add('error');
   li.innerText = "Movie not found!";
   resCont.append(li);
@@ -61,19 +55,46 @@ const renderDetails = (obj) => {
 }
 
 
+const renderFavourites = (favTitle) => {
+  const f = document.createElement('p');
+  f.innerText = `${favTitle}`;
+  fMovies.append(f);
+}
 
-const showRes = (arr) => {
+const showFavContainer = () => {
+  fMovies.innerHTML = "";
+  let favArr = [...Object.keys(localStorage)];
+  favArr.forEach((item) => {
+     const p = document.createElement('p');
+     p.innerText = `${item}`;
+     fMovies.append(p);
+  })
+}
+
+
+showFav.addEventListener('click', showFavContainer);
+
+
+const showResults = (arr) => {
   arr.forEach( ({Title, Year, Type, imdbID}) => {
       const li = document.createElement('li');
       li.classList.add('film');
       li.innerText = `"${Title}", ${Type}, ${Year}`;
       resCont.append(li);
+
+      const addButton = document.createElement('button');
+      addButton.classList.add('add-to-fav');
+      addButton.innerHTML = "<span class='star'>&#9733;</span>";
+      addButton.setAttribute("data-fav", `${imdbID}`);
+      li.append(addButton);
+      addButton.addEventListener('click', addToFavourites); 
+
       const dtls = document.createElement('button');
       dtls.classList.add('details-btn');
       dtls.innerText = "details";
       dtls.setAttribute("data-id", `${imdbID}`);
       li.append(dtls);
-      dtls.addEventListener('click', showDetails);
+      dtls.addEventListener('click', showDetails); 
   });
 }
 
@@ -104,16 +125,18 @@ const showPages = (resultNum) => {
 
 async function getResponse(p = 1) {
   const ttl = document.querySelector('.search-inp').value;
+  const t = document.querySelector('.type-select').value;
 
 
   resCont.innerHTML = "";
   pagination.innerHTML = "";
+  detailCont.classList.remove('visible');
   
     try {
       const resp = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&type=${t}&s=${ttl}&page=${p}`)
       const data = await resp.json()
       const pgNm = await data.totalResults;
-      showRes(data.Search);
+      showResults(data.Search);
       showPages(pgNm);
     } catch (e) {
       showError();
@@ -137,3 +160,18 @@ async function showDetails (e) {
   renderDetails(dt);
 }
 
+async function addToFavourites (e) {
+  const favMovie = this.getAttribute("data-fav");
+  const favResp = await fetch(`https://www.omdbapi.com/?apikey=${apikey}&i=${favMovie}`)
+  const favDet = await favResp.json()
+  window.localStorage.setItem(JSON.stringify(favDet.Title), JSON.stringify(favDet.imdbID));
+  renderFavourites(favDet.Title);
+}
+
+showFav.addEventListener('click', function() {
+  favBlock.classList.toggle('visible');
+})
+
+clearBtn.addEventListener('click', function() {
+  localStorage.clear();
+})
