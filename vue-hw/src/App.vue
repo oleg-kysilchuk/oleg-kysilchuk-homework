f5cef7206e7945893f15871aaac3427acd21afde
oleg-kysilchuk <oleh.kysilchuk@gmail.com>
2020-08-14 20:49:29 +0300

    vue-hw3

---
+++ vue-hw/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Comp />
+  </div> 
+</template>
+
+<script>
+
+  import Comp from '@/components/Comp'
+
+  export default {
+    name: 'app',
+    components: {
+      Comp
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-image: url("./assets/bg.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    overflow: hidden;
+  }
+
+  #app {
+    width: 100vw;
+    height: 100vh;
+  }
+
+</style>
---
+++ vue-hw/src/components/Comp.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container">
+        <input ref="inp" type="number" v-model="num" @blur="getNum">
+        <p ref="res" class="result">{{curNum}}</p>
+        <button type="button" @click="incNum">increase</button>
+        <button type="button" @click="decNum">decrease</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num: null,
+            curNum: null,
+        }
+    },
+
+    methods: {
+        getNum() {
+            this.curNum = this.num;
+            this.$refs.res.innerText = this.curNum;
+            this.num = '';
+        },
+
+        incNum() {
+            this.curNum++;
+            this.$refs.res.innerText = this.curNum;
+        },
+
+        decNum() {
+            this.curNum--;
+            this.$refs.res.innerText = this.curNum;
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .container {
+        margin: 10% auto 0;
+        width: 200px;
+        height: 150px;
+        border: 2px solid white;
+        border-radius: 15px;
+        padding: 2% 1%;
+    }
+
+    input {
+        width: 100%;
+        outline: none;
+        margin: 5px 0 10px;
+        border-radius: 10px;
+        text-align: center;
+    }
+
+    .result {
+        margin: 0 auto;
+        width: 100%;
+        background-color: #ffffff;
+        color: black;
+        height: 16px;
+        margin-bottom: 10px;
+        border-radius: 10px;
+        text-align: center;
+    }
+
+    button {
+        display: block;
+        width: 100%;
+        margin-bottom: 5px;
+        background-color: blue;
+        color: white;
+        text-transform: uppercase;
+        border-radius: 10px;
+        outline: none;
+    }
+
+    button:last-of-type {
+        background-color: red;
+    }
+
+</style>
